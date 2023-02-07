0ad2f2b8c74b43d770386b07bdf367831e42e91e
Ryan Yee <ryee2@wisc.edu>
2023-02-07 14:17:37 -0600

    added get_gain_ratio function to calculate information entroy gain ratio

--- homework2/code/tree.py
+++ homework2/code/tree.py
@@ -10,7 +10,10 @@ def make_subtree(data):
         pass # make leaf
     elif len(C) == 0:
         pass # make leaf
+    else:
+        S = find_best_split(data, C)
     pass
+
 
 def get_info_entropy(data):
     '''
@@ -21,6 +24,7 @@ def get_info_entropy(data):
     p1 = n1 / (n1 + n0)
     p0 = n0 / (n1 + n0)
     return -p0 * math.log2(p0) - p1 * math.log2(p1)
+
 
 def get_info_entropy_gain(data, c, j):
     '''
@@ -34,6 +38,16 @@ def get_info_entropy_gain(data, c, j):
     hy_x = p_less * hy_less + p_gtet * hy_gtet
     return hy - hy_x
 
+
+def get_gain_ratio(data, c, j):
+    '''
+    returns entropy gain ratio from splitting at cut-point c on variable j
+    '''
+    p_less = len(data[data.iloc[:, j] < c]) / len(data)
+    p_gtet = len(data[data.iloc[:, j] >= c]) / len(data)
+    return get_info_entropy_gain(data, c, j) / (-p_less * math.log2(p_less) - p_gtet * math.log2(p_gtet))
+
+
 def get_one_var_splits(data, j):
     '''
     returns splits for variable j
@@ -42,18 +56,21 @@ def get_one_var_splits(data, j):
     for i in range(len(data.iloc[:, j])):
         try:
             entropy_gain = get_info_entropy_gain(data, data.iloc[:, j][i], j)
+            gain_ratio = get_gain_ratio(data, data.iloc[:, j][i], j)
         except:
             pass
         else:
-            if entropy_gain > 0:
+            if entropy_gain > 0 & gain_ratio > 0:
                 C.append([data.iloc[:, j][i], j])
     return C
+
 
 def get_candidate_splits(data):
     '''
     returns 2D array of candidate splits c and corresponding variable index j
     '''
     return get_one_var_splits(data, 0) + get_one_var_splits(data, 1)
+
 
 def find_best_split(data, candidates_splits):
     max_gain = 0
@@ -69,5 +86,7 @@ def find_best_split(data, candidates_splits):
                 max_index = i
     return candidates_splits[max_index]
 
+
 if __name__=="__main__":
     print(len(get_candidate_splits(d1)))
+
