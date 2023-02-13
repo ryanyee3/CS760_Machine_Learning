aed9999a4878ec1ec734d747f87acb327dbb2de6
Ryan Yee <ryee2@wisc.edu>
2023-02-13 12:08:51 -0600

    updates for question 4

--- homework2/code/lagrange.py
+++ homework2/code/lagrange.py
@@ -3,16 +3,22 @@ from matplotlib import pyplot as plt
 from scipy.interpolate import lagrange
 from numpy.polynomial.polynomial import Polynomial
 
-def lagrange_experiment(a, b, n):
+def lagrange_experiment(a, b, n, sigma):
     train = np.sort(np.random.uniform(low=a, high=b, size=n))
-    poly = lagrange(train, np.sin(train))
+    epsilon = np.random.normal(loc=0, scale=sigma, size=n)
+    poly = lagrange(train + epsilon, np.sin(train))
     test = np.random.uniform(low=a, high=b, size=n)
     train_error = sum((Polynomial(poly.coef[::-1])(train) - np.sin(train)) ** 2) / n
     test_error = sum((Polynomial(poly.coef[::-1])(test) - np.sin(test)) ** 2) / n
     print(n, " & ", train_error, " & ", test_error, " \\\\")
 
 if __name__=="__main__":
-    lagrange_experiment(-3, 3, 10)
-    lagrange_experiment(-3, 3, 20)
-    lagrange_experiment(-3, 3, 50)    
+    lagrange_experiment(-3, 3, 10, 0)
+    lagrange_experiment(-3, 3, 20, 0)
+    lagrange_experiment(-3, 3, 50, 0)
+    print("/n")
+    lagrange_experiment(-3, 3, 15, .5)
+    lagrange_experiment(-3, 3, 15, 1)
+    lagrange_experiment(-3, 3, 15, 2)
+    lagrange_experiment(-3, 3, 15, 5) 
     pass
