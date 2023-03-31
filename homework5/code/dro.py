1d82e7db365fc3ab16d7518ceae56ad655bfa589
Ryan Yee <ryee2@wisc.edu>
2023-03-31 11:41:14 -0500

    moved experiments to if __name__='__main__':

--- homework5/code/dro.py
+++ homework5/code/dro.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pca import plot_2d_pca, get_reconstruction_error
 
 data2d = np.loadtxt("homework5/data/data2d.csv", delimiter=',') # shape (50, 2)
 data1000d = np.loadtxt("homework5/data/data1000d.csv", delimiter=',') # shape (500, 1000)
@@ -14,24 +15,18 @@ def dro(X, k):
     Z = np.matmul(Q, A)
     return b, A, Z
 
-def plot_2d_pca(X, re_X):
-    plt.scatter(X[:, 0], X[:, 1], c="blue", marker='x')
-    plt.scatter(re_X[:, 0], re_X[:, 1], c="red", marker='+')
-
-def get_reconstruction_error(X, re_X):
-    return round((np.linalg.norm(X - re_X)**2) / len(X), 6)
-
-# experiment
-b2d, A2d, Z2d = dro(data2d, 1)
-X2d = np.matmul(Z2d, A2d.T) + b2d
-print(get_reconstruction_error(data2d, X2d))
-plot_2d_pca(data2d, X2d)
-plt.title("DRO")
-plt.savefig("homework5/figs/dro.png")
-
-b1000d, A1000d, Z1000d = dro(data1000d, 500)
-X1000d = np.matmul(Z1000d, A1000d.T) + b1000d
-print(get_reconstruction_error(data1000d, X1000d))
-
 if __name__ == '__main__':
+    # experiment
+
+    b2d, A2d, Z2d = dro(data2d, 1)
+    X2d = np.matmul(Z2d, A2d.T) + b2d
+    print(get_reconstruction_error(data2d, X2d))
+    plot_2d_pca(data2d, X2d)
+    plt.title("DRO")
+    plt.savefig("homework5/figs/dro.png")
+
+    b1000d, A1000d, Z1000d = dro(data1000d, 500)
+    X1000d = np.matmul(Z1000d, A1000d.T) + b1000d
+    print(get_reconstruction_error(data1000d, X1000d))
+    
     pass
--- homework5/code/pca.py
+++ homework5/code/pca.py
@@ -40,66 +40,67 @@ def plot_2d_pca(X, re_X):
 def get_reconstruction_error(X, re_X):
     return round((np.linalg.norm(X - re_X)**2) / len(X), 6)
 
-# experiment
 
-# 2D Data
-# bv, bz, bx = buggy_pca(data2d, 1)
-# print(get_reconstruction_error(data2d, bx))
-# plot_2d_pca(data2d, bx)
-# plt.title("Buggy PCA")
-# plt.savefig("homework5/figs/buggy_pca.png")
-# plt.clf()
-
-# dv, dz, dx = demeaned_pca(data2d, 1)
-# print(get_reconstruction_error(data2d, dx))
-# plot_2d_pca(data2d, dx)
-# plt.title("Demeaned PCA")
-# plt.savefig("homework5/figs/demeaned_pca.png")
-# plt.clf()
-
-# nv, nz, nx = normalized_pca(data2d, 1)
-# print(get_reconstruction_error(data2d, nx))
-# plot_2d_pca(data2d, nx)
-# plt.title("Normalized PCA")
-# plt.savefig("homework5/figs/normalized_pca.png")
-# plt.clf()
-
-# 1000D Data
-# k = [100, 200, 300, 400, 500, 600, 700, 800, 900]
-# buggy_rc_error = []
-# demeaned_rc_error = []
-# normalized_rc_error = []
-# for i in range(len(k)):
-#     bv, bz, bx = buggy_pca(data1000d, k[i])
-#     buggy_rc_error.append(get_reconstruction_error(data1000d, bx))
-#     dv, dz, dx = demeaned_pca(data1000d, k[i])
-#     demeaned_rc_error.append(get_reconstruction_error(data1000d, dx))
-#     nv, nz, nx = normalized_pca(data1000d, k[i])
-#     normalized_rc_error.append(get_reconstruction_error(data1000d, nx))
-    
-# plt.plot(k, buggy_rc_error)
-# plt.title("Reconstruction Error vs. Principal Components for Buggy PCA")
-# plt.savefig("homework5/figs/buggy_error_curve.png")
-# plt.clf()
-
-# plt.plot(k, demeaned_rc_error)
-# plt.title("Reconstruction Error vs. Principal Components for Demeaned PCA")
-# plt.savefig("homework5/figs/demeaned_error_curve.png")
-# plt.clf()
-
-# plt.plot(k, normalized_rc_error)
-# plt.title("Reconstruction Error vs. Principal Components for Normalized PCA")
-# plt.savefig("homework5/figs/normalized_error_curve.png")
-# plt.clf()
-
-bv, bz, bx = buggy_pca(data1000d, 500)
-print(get_reconstruction_error(data1000d, bx))
-
-dv, dz, dx = demeaned_pca(data1000d, 500)
-print(get_reconstruction_error(data1000d, dx))
-
-nv, nz, nx = normalized_pca(data1000d, 500)
-print(get_reconstruction_error(data1000d, nx))
 
 if __name__ == '__main__':
+    # experiment
+
+    # 2D Data
+    bv, bz, bx = buggy_pca(data2d, 1)
+    print(get_reconstruction_error(data2d, bx))
+    plot_2d_pca(data2d, bx)
+    plt.title("Buggy PCA")
+    plt.savefig("homework5/figs/buggy_pca.png")
+    plt.clf()
+
+    dv, dz, dx = demeaned_pca(data2d, 1)
+    print(get_reconstruction_error(data2d, dx))
+    plot_2d_pca(data2d, dx)
+    plt.title("Demeaned PCA")
+    plt.savefig("homework5/figs/demeaned_pca.png")
+    plt.clf()
+
+    nv, nz, nx = normalized_pca(data2d, 1)
+    print(get_reconstruction_error(data2d, nx))
+    plot_2d_pca(data2d, nx)
+    plt.title("Normalized PCA")
+    plt.savefig("homework5/figs/normalized_pca.png")
+    plt.clf()
+
+    # 1000D Data
+    k = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+    buggy_rc_error = []
+    demeaned_rc_error = []
+    normalized_rc_error = []
+    for i in range(len(k)):
+        bv, bz, bx = buggy_pca(data1000d, k[i])
+        buggy_rc_error.append(get_reconstruction_error(data1000d, bx))
+        dv, dz, dx = demeaned_pca(data1000d, k[i])
+        demeaned_rc_error.append(get_reconstruction_error(data1000d, dx))
+        nv, nz, nx = normalized_pca(data1000d, k[i])
+        normalized_rc_error.append(get_reconstruction_error(data1000d, nx))
+        
+    plt.plot(k, buggy_rc_error)
+    plt.title("Reconstruction Error vs. Principal Components for Buggy PCA")
+    plt.savefig("homework5/figs/buggy_error_curve.png")
+    plt.clf()
+
+    plt.plot(k, demeaned_rc_error)
+    plt.title("Reconstruction Error vs. Principal Components for Demeaned PCA")
+    plt.savefig("homework5/figs/demeaned_error_curve.png")
+    plt.clf()
+
+    plt.plot(k, normalized_rc_error)
+    plt.title("Reconstruction Error vs. Principal Components for Normalized PCA")
+    plt.savefig("homework5/figs/normalized_error_curve.png")
+    plt.clf()
+
+    bv, bz, bx = buggy_pca(data1000d, 500)
+    print(get_reconstruction_error(data1000d, bx))
+
+    dv, dz, dx = demeaned_pca(data1000d, 500)
+    print(get_reconstruction_error(data1000d, dx))
+
+    nv, nz, nx = normalized_pca(data1000d, 500)
+    print(get_reconstruction_error(data1000d, nx))
     pass
